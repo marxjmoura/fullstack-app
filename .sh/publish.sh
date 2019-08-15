5a2993a33735824ac5e502226790803ed5b5952f
Marx JMoura <marx.jmoura@gmail.com>
2019-08-15 14:19:27 -0300

    Shell script to publish

---
+++ .sh/publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install
+npm run build
+git checkout --orphan gh-pages
+ls -A | grep -v ".git\|dist" | xargs rm -rf
+cp -r dist/* .
+echo "fullstack.app" > CNAME
+git add .
+git commit -m "Publish"
+git push -f gh-pages
