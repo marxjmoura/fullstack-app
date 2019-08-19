4494a020d403eab943bba4602f2584646ca4d3ea
Marx JMoura <marx.jmoura@gmail.com>
2019-08-19 12:48:26 -0300

    Exlude .gitignore from gh-pages

--- .sh/publish.sh
+++ .sh/publish.sh
@@ -9,7 +9,7 @@ git pull origin master
 npm install
 npm run build
 git checkout --orphan gh-pages
-ls -A | grep -v ".git\|dist" | xargs rm -rf
+ls -A | grep -v ".git$\|dist" | xargs rm -rf
 cp -r dist/* .
 echo "fullstack.app" > CNAME
 git add .
