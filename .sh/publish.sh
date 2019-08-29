349181b7bf0c0c584154a03046c041cf69c4ba7d
Marx JMoura <marx.jmoura@gmail.com>
2019-08-28 21:30:02 -0300

    Improve publish script

--- .sh/publish.sh
+++ .sh/publish.sh
@@ -4,15 +4,25 @@ set -e
 
 git checkout master
 git pull origin master
+
 rm -rf dist
-[[ -d "./gh-pages" ]] && git worktree remove ./gh-pages
-[[ `git branch --list gh-pages` ]] && git branch -D gh-pages
-git worktree add ./gh-pages gh-pages
+
 npm install
 npm run build
-rm -rf ./gh-pages/*
+
+if [[ `git branch --list gh-pages` ]] then
+  git worktree remove ./gh-pages --force
+  git branch -D gh-pages
+fi
+
+rm -rf ./gh-pages
+git worktree add ./gh-pages gh-pages
+
 cp -r ./dist/* ./gh-pages/
 echo "fullstack.app" > ./gh-pages/CNAME
+
 cd ./gh-pages
-git commit -am "Publish"
+
+git add .
+git commit -m "Publish"
 git push -f origin gh-pages
